991b945830a2ab449f8ed48cda2c12c80736b89c
Bruno Ferreira <126132486+brunoferreiraadev@users.noreply.github.com>
2023-06-22 01:08:14 -0300

    Add files via upload

--- javascript/pagina.js
+++ javascript/pagina.js
@@ -1,7 +1,33 @@
-window.addEventListener('scroll', function () {
-    let header = this.document.querySelector('#header')
-    header.classList.toggle('rolagem', this.window.scrollY > 0)
-})
+// window.addEventListener('scroll', function () {
+//     let header = this.document.querySelector('#header')
+//     header.classList.toggle('rolagem', this.window.scrollY > 0)
+// })
+
+window.addEventListener('load', function () {
+    function handleScroll() {
+        let header = document.querySelector('#header');
+        header.classList.toggle('rolagem', window.scrollY > 0);
+    }
+
+    function addScrollListener() {
+        window.addEventListener('scroll', handleScroll);
+    }
+
+    function removeScrollListener() {
+        window.removeEventListener('scroll', handleScroll);
+    }
+
+    function handleResize() {
+        if (window.innerWidth > 769) {
+            addScrollListener();
+        } else {
+            removeScrollListener();
+        }
+    }
+
+    window.addEventListener('resize', handleResize);
+    handleResize();
+});
 
 window.addEventListener('beforeunload', function () {
     window.scrollTo(0, 0);
